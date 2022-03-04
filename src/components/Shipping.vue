1cad539868f707d089a22b4c2ccf71b5242c48a0
husnuljahneer <husnuljahneer19@gmail.com>
2022-03-04 17:48:45 +0530

    1.5 Added Shipping Component

---
+++ src/components/Shipping.vue
@@ -0,0 +1,150 @@
+<template>
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-row flex-wrap -m-4">
+        <div class="flex-col p-4 lg:w-1/3 items-center">
+          <div class="px-8 overflow-hidden border-r-2">
+            <div class="flex justify-center mb-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                class="bi bi-truck text-yellow-600"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+                />
+              </svg>
+            </div>
+            <h2
+              class="
+                tracking-widest
+                text-xs
+                title-font
+                font-medium
+                text-gray-400
+                mb-1
+              "
+            >
+              FREE SHIPPING
+            </h2>
+            <h1
+              class="
+                title-font
+                sm:text-2xl
+                text-xl
+                font-medium
+                text-gray-900
+                mb-3
+              "
+            >
+              On all orders above 50$
+            </h1>
+          </div>
+        </div>
+
+        <div class="flex-col p-4 lg:w-1/3 items-center">
+          <div class="px-8 overflow-hidden border-r-2">
+            <div class="flex justify-center mb-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                class="bi bi-telephone-plus text-yellow-600"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M12.5 1a.5.5 0 0 1 .5.5V3h1.5a.5.5 0 0 1 0 1H13v1.5a.5.5 0 0 1-1 0V4h-1.5a.5.5 0 0 1 0-1H12V1.5a.5.5 0 0 1 .5-.5z"
+                />
+              </svg>
+            </div>
+            <h2
+              class="
+                tracking-widest
+                text-xs
+                title-font
+                font-medium
+                text-gray-400
+                mb-1
+              "
+            >
+              FREE SUPPORT
+            </h2>
+            <h1
+              class="
+                title-font
+                sm:text-2xl
+                text-xl
+                font-medium
+                text-gray-900
+                mb-3
+              "
+            >
+              24/7 call center available
+            </h1>
+          </div>
+        </div>
+
+        <div class="flex-col p-4 lg:w-1/3 items-center">
+          <div class="px-8 overflow-hidden">
+            <div class="flex justify-center mb-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                class="bi bi-skip-backward-circle text-yellow-600"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+                />
+                <path
+                  d="M11.729 5.055a.5.5 0 0 0-.52.038L8.5 7.028V5.5a.5.5 0 0 0-.79-.407L5 7.028V5.5a.5.5 0 0 0-1 0v5a.5.5 0 0 0 1 0V8.972l2.71 1.935a.5.5 0 0 0 .79-.407V8.972l2.71 1.935A.5.5 0 0 0 12 10.5v-5a.5.5 0 0 0-.271-.445z"
+                />
+              </svg>
+            </div>
+            <h2
+              class="
+                tracking-widest
+                text-xs
+                title-font
+                font-medium
+                text-gray-400
+                mb-1
+              "
+            >
+              FREE RETURN
+            </h2>
+            <h1
+              class="
+                title-font
+                sm:text-2xl
+                text-xl
+                font-medium
+                text-gray-900
+                mb-3
+              "
+            >
+              30 day return policy
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
