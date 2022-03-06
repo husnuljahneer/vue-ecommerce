37876e3e75200653c4a24015fdea01f1cff56447
husnuljahneer <husnuljahneer19@gmail.com>
2022-03-07 01:07:39 +0530

    4.2 Added VuexPersist Plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
 import { createStore } from 'vuex'
 import productModule from './modules/products'
 import cartModule from './modules/cart'
+
+import VuexPersist from "vuex-persist";
+
+const vuexPersist = new VuexPersist({
+    key: "ecom-stores",
+    storage: window.localStorage,
+});
+
 export default createStore({
     modules: {
         products: productModule,
         cart: cartModule,
-    }
+    },
+    plugins: [vuexPersist.plugin],
 })
