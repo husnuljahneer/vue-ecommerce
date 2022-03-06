38da14d932e7f11834882ca7ec20953a0ed73207
husnuljahneer <husnuljahneer19@gmail.com>
2022-03-06 23:40:03 +0530

    3.2 Added all the product functions to the store

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -1,32 +1,47 @@
-import axios from 'axios';
+import axios from "axios";
 import baseUrl from '../../Services/Base';
 
 const state = () => ({
     products: [],
     product: [],
-})
+});
 
 const getters = {
     products: (state) => state.products,
-}
+};
 
 const actions = {
     getAllProducts({ commit }) {
-        axios.get(baseUrl + "products").then((response) => {
-            commit("SET_PRODUCTS", response.data.products);
+        axios.get(baseUrl + "products")
+            .then((response) => {
+                commit("SET_PRODUCTS", response.data.products);
+            });
+    },
+    getProductById({ commit }, id) {
+        axios.get(baseUrl + `products/${id}`).then((response) => {
+            commit("SET_PRODUCT", response.data.product);
         });
     },
-}
+    getUserProducts({ commit }, id) {
+        axios.get(baseUrl + `products/userProduct/${id}`).then((response) => {
+            commit("GET_USER_PRODUCTS", response.data.products);
+        });
+    }
+};
+
 const mutations = {
     SET_PRODUCTS(state, products) {
         state.products = products;
     },
-}
+    SET_PRODUCT(state, product) {
+        state.product = product;
+    },
+};
 
 export default {
     namespaced: true,
     state,
+    actions,
     getters,
     mutations,
-    actions
-}
+};
