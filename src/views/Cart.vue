f4c2c1c92af98451b32465fa4d8124b2312ebe02
husnuljahneer <husnuljahneer19@gmail.com>
2022-03-07 01:04:04 +0530

    4.1 Added Empty Cart Section

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -267,6 +267,35 @@
       </div>
     </div>
   </section>
+  <section class="text-gray-600 body-font" v-else>
+    <div
+      class="
+        container
+        mx-auto
+        flex flex-col
+        px-5
+        py-24
+        justify-center
+        items-center
+      "
+    >
+      <img
+        class="lg:w-1/5 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+        alt="hero"
+        src="https://i.imgur.com/ztqw8L5.png"
+      />
+      <div class="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
+        <h1
+          class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"
+        >
+          Your cart is empty
+        </h1>
+        <p class="mb-8 leading-relaxed">
+          You have no items in your shopping cart. Let's go buy something.
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
   <script>
