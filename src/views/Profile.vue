c4399946024ecd3d86dfffd50401ffa6675943f7
husnuljahneer <husnuljahneer19@gmail.com>
2022-03-09 18:15:05 +0530

    8.1 Made Invoice View Responsive

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <section
+  <section class="hidden md:block"
     v-if="this.userProducts.length > 1"
     style="background-color: #100d07; min-height: 60vh"
   >
@@ -174,6 +174,134 @@
                 >
                   <p class="text-green-700 whitespace-no-wrap">Shipped</p>
                 </td>
+                <td
+                  class="
+                    px-5
+                    py-5
+                    border-b
+                    text-left
+                    border-gray-200
+                    bg-white
+                    text-sm
+                  "
+                >
+                  <router-link
+                    :to="{
+                      name: 'Invoice',
+                      params: { order_id: item.order_id },
+                    }"
+                  >
+                    <p class="text-blue-700 whitespace-no-wrap font-semibold">
+                      View
+                    </p>
+                  </router-link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section
+  class="flex md:hidden"
+    v-if="this.userProducts.length > 1"
+    style="background-color: #100d07; min-height: 60vh"
+  >
+    <div class="flex flex-col pb-6 h-window p-10">
+      <div class="flex flex-col">
+        <p class="text-md text-left text-white w-3/4 font-semibold">
+          Hi,
+          <span class="text-yellow-600 font-normal uppercase">{{
+            userEmail.split("@")[0]
+          }}</span>
+        </p>
+        <h1
+          class="
+            text-md text-left
+            sm:text-lg
+            md:text-3xl
+            lg:text3xl
+            text-white
+            font-serif
+          "
+        >
+          Order History
+        </h1>
+      </div>
+      <div class="-mx-4 sm:-mx-8 w-full px-0 sm:px-4 md:px-4 lg:px-4 py-10">
+        <div class="inline-block w-full overflow-hidden">
+          <table class="min-w-full leading-normal">
+            <thead>
+              <tr>
+                <th
+                  class="
+                    rounded-tl-2xl
+                    px-5
+                    py-4
+                    border-b-2 border-gray-200
+                    text-left text-xs text-yellow-500
+                    font-bold
+                    tracking-wider
+                  "
+                >
+                  #Order Id
+                </th>
+                <th
+                  class="
+                    px-5
+                    py-4
+                    border-b-2 border-gray-200
+                    text-left text-xs text-yellow-500
+                    font-bold
+                    tracking-wider
+                  "
+                >
+                  Product Name
+                </th>
+                <th
+                  class="
+                    rounded-tr-2xl
+                    px-5
+                    py-4
+                    border-b-2 border-gray-200
+                    text-left text-xs text-yellow-500
+                    font-bold
+                    tracking-wider
+                  "
+                >
+                  Invoice
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in userProducts" :key="item.id">
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                  <div class="flex items-center">
+                    <div class="ml-3">
+                      <p class="text-black font-bold whitespace-no-wrap">
+                        {{ item.order_id }}
+                      </p>
+                    </div>
+                  </div>
+                </td>
+                <td
+                  class="
+                    px-5
+                    py-5
+                    border-b
+                    text-left
+                    border-gray-200
+                    bg-white
+                    text-sm
+                  "
+                >
+                  <p class="text-black whitespace-no-wrap">
+                    {{ item.product_name }}
+                  </p>
+                </td>
+               
                 <td
                   class="
                     px-5
